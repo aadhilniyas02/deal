03051b442ae98001904d477974bbf45b413548c2
Aadhil <mohomedaadhil0404@gmail.com>
2025-02-17 15:54:32 +0530

    .

---
+++ dealSystem/Migrations/20250217101242_HotelsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dealSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- dealSystem/obj/Debug/net8.0/dealSystem.AssemblyInfo.cs
+++ dealSystem/obj/Debug/net8.0/dealSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dealSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44ede9494aeee726860e43082cc0a5d938664b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c8d8269ea38357ad3579cf04664b1b5cf2c9a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("dealSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dealSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
